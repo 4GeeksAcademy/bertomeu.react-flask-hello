07cc19ecb8f3c3bab4f6e145f198cc6f26bbb535
bertomeu <josecarlosbertomeu@gmail.com>
2024-09-02 15:14:25 +0000

    modified layout.js

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-import { BackendURL } from "./component/backendURL";
+
 
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
+import Profile from "./pages/profile";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import NavbarTop from "./component/navbarTop";
 
 //create your first component
 const Layout = () => {
@@ -17,18 +19,19 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    
 
     return (
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
+                    <NavbarTop />
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<h1>Not found!</h1>} />
+                        <Route element={<Home />} path="/" />                     
+                        <Route element={<Profile />} path="/profile" />
+                        <Route element={<Register />} path="/register" />
+                        <Route element={<Login />} path="/login" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
