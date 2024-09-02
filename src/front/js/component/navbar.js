360e95e5d8f4cd4d1dd024aad41fafeb96f306c3
bertomeu <josecarlosbertomeu@gmail.com>
2024-09-02 15:15:22 +0000

    modified navbar.js

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
+import { Context } from '../store/appContext';
 
 export const Navbar = () => {
+
+	const { store, actions } = useContext(Context);
+
+	const handleLogout = () => {
+		actions.logout();
+	};
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+		<nav className="navbar navbar-light bg-success-subtle">
+			<div className="container-fluid justify-content-evenly">
+				{
+					store.user ?
+						<div>
+							<Link to="/">
+								<span className="btn btn-outline-primary" onClick={handleLogout}>Log out</span>
+							</Link>
+						</div> :
+						<>
+							<div>
+								<Link to="/login">
+									<span className="btn btn-outline-primary">Login</span>
+								</Link>
+							</div>
+							<div>
+								<Link to="/register">
+									<button className="btn btn-outline-primary">Sign up</button>
+								</Link>
+							</div>
+						</>
+				}
+
 			</div>
 		</nav>
 	);
